5310ac22477cfc6caa8a5b1a89e6ebfc938379d4
Henrique <washington.sousa01@fatec.sp.gov.br>
2023-04-16 12:19:14 -0300

    feat: #1 base project structure to start

---
+++ src/pages/Main/index.page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Main = () => {
+  return <>Hello From Main</>
+}
+
+export default Main;
---
+++ src/shared/components/TopBar/index.tsx
@@ -0,0 +1,9 @@
+import Logo from "@/shared/assets/logos/logo-secundary.svg";
+
+import { Container } from "./styles";
+
+const TopBar = () => {
+  return <Container></Container>;
+};
+
+export default TopBar;
